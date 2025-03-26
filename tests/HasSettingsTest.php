7ff486805d19988c9439317ea9a32834b39818f7
iuriemalai <10517115+iuriemalai@users.noreply.github.com>
2025-03-26 15:01:44 +0000

    Fix styling

--- tests/HasSettingsTest.php
+++ tests/HasSettingsTest.php
@@ -4,8 +4,8 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
 
 class HasSettingsTest extends TestCase
 {
@@ -15,9 +15,9 @@ class HasSettingsTest extends TestCase
     public function it_can_set_a_user_setting()
     {
         $user = User::factory()->create();
-        
+
         $user->setSetting('theme', 'dark');
-        
+
         $this->assertEquals('dark', $user->getSetting('theme'));
         $this->assertDatabaseHas('users', [
             'id' => $user->id,
@@ -29,7 +29,7 @@ class HasSettingsTest extends TestCase
     public function it_can_get_a_user_setting()
     {
         $user = User::factory()->create(['settings' => ['lang' => 'en']]);
-        
+
         $this->assertEquals('en', $user->getSetting('lang'));
     }
 
@@ -37,7 +37,7 @@ class HasSettingsTest extends TestCase
     public function it_returns_null_if_setting_does_not_exist()
     {
         $user = User::factory()->create();
-        
+
         $this->assertNull($user->getSetting('nonexistent_key'));
     }
 
@@ -45,9 +45,9 @@ class HasSettingsTest extends TestCase
     public function it_can_remove_a_user_setting()
     {
         $user = User::factory()->create(['settings' => ['lang' => 'en', 'theme' => 'dark']]);
-        
+
         $user->removeSetting('lang');
-        
+
         $this->assertNull($user->getSetting('lang'));
         $this->assertEquals(['theme' => 'dark'], $user->settings);
     }
@@ -56,9 +56,9 @@ class HasSettingsTest extends TestCase
     public function it_preserves_other_settings_when_removing_a_setting()
     {
         $user = User::factory()->create(['settings' => ['lang' => 'fr', 'mode' => 'light']]);
-        
+
         $user->removeSetting('lang');
-        
+
         $this->assertNull($user->getSetting('lang'));
         $this->assertEquals(['mode' => 'light'], $user->settings);
     }
